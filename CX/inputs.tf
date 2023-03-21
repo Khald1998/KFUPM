3071b2e776680570ba3c384aad6c9b883b57bd73
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-03-21 13:47:19 +0300

    -

--- CX/inputs.tf
+++ CX/inputs.tf
@@ -12,3 +12,12 @@ variable "subnet_cidr_block" {
   type    = string
   default = "10.0.0.0/24"
 }
+
+variable "EC2_name" {
+  type    = string
+  default = "web-app"
+}
+variable "repository_name" {
+  type    = string
+  default = "my_repository"
+}
