239f4cd844c2e2becd4e47e9c3247861905b665c
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-12-04 02:36:48 +0300

    -

--- SP/Backend/microcontroller.py
+++ SP/Backend/microcontroller.py
@@ -5,12 +5,8 @@ from flask import jsonify, request
 def microcontrollerPOST():
     req = request.json
     collection = mongo.db.sensors
-
-    # Prepare the update data with the correct key transformation and path
     nested_sensor_data = {"sensor_data." + key.lower().replace("tank", "_tank").replace("water", "_water").replace("temp", "_temp").replace("volume", "_volume"): value for key, value in req.items()}
-    # Update the document with the nested structure
     collection.update_one({"_id": "sensor_states"}, {"$set": nested_sensor_data}, upsert=True)
-
     print("Processed sensors:", req)
     return jsonify({"message": "All sensors processed successfully"}), 200
 
@@ -21,14 +17,17 @@ def microcontrollerPOST():
 @app.route('/microGET', methods=['GET'])
 def microcontrollerGET():
     collection = mongo.db.sensors
-
     sensor_states_doc = collection.find_one({"_id": "sensor_states"})
+
+    # Extract the sensor data correctly
     if sensor_states_doc:
-        sensor_states = sensor_states_doc.get("sensors", {})
+        # Assuming all sensor data is nested under "sensor_data"
+        sensor_states = sensor_states_doc.get("sensor_data", {})
     else:
         sensor_states = {}
 
     return jsonify(sensor_states), 200
+
 
 
 
